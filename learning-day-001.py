fa7a0fe8a86822e5f89c1db4622d6b3728e415a9
masud001 <madhnagar@gmail.com>
2026-07-17 17:37:24 +0600

    udpate code

--- learning-day-001.py
+++ learning-day-001.py
@@ -1,9 +1,21 @@
-x= "Masud"
+a= "this is masud"
 
-def func():
-    global y
-    y = "jaya"
+# loop
+# for x in a:
+#     print(x)
 
-func()
-print(y)
-print(x)
+
+
+print( "masud" in a)
+print("is" not in a)
+
+# check words or character by if...else
+y = "masud"
+if y in a:
+    print("this is true :",y)
+else:
+    print("this is not masud")
+
+
+
+
